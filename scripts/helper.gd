bf993fbb008e38238ebee3f82bcaeb9867b290a9
Mike Watt <64022360+mmbwatt@users.noreply.github.com>
2021-04-19 21:37:06 -0400

    Delete scripts directory
    delete scripts to correct naming convention of Scripts

--- scripts/helper.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-
-func BuildChunkName(position: Vector3) -> String:
-	return str(position.x) + "_" + str(position.y) + "_" + str(position.z)
-
-const chunk_size := 8
-const column_height := 8
